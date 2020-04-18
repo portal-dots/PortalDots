701d06fe6be848fb6a9100a986260e128a506b64
hosakou <hosa_kou@hotmail.co.jp>
2020-04-18 14:56:46 +0900

    Request クラスの名前空間を変更

--- app/Http/Controllers/Staff/Forms/Admins/StoreAction.php
+++ app/Http/Controllers/Staff/Forms/Admins/StoreAction.php
@@ -7,7 +7,7 @@ use App\Eloquents\Form;
 use App\Eloquents\FormAdministrator;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Forms\Admins\StoreRequest;
+use App\Http\Requests\Staff\Forms\Admins\StoreRequest;
 
 class StoreAction extends Controller
 {
--- app/Http/Requests/Staff/Forms/Admins/StoreRequest.php
+++ app/Http/Requests/Staff/Forms/Admins/StoreRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\Forms\Admins;
+namespace App\Http\Requests\Staff\Forms\Admins;
 
 use Illuminate\Foundation\Http\FormRequest;
 use App\Eloquents\User;
